17ac92d66af45614ca02911ba085a9565d0ce2b7
Gary Lam <garylam5678@gmail.com>
2018-07-07 21:22:30 -0400

    Completed registers_m w/ other changes in description
    Added non-blocking assignments to the decoder module
    Added test cases from project assignment test file along with variables to test the register module

--- test.v
+++ test.v
@@ -1,27 +1,40 @@
 `include "decoder_m.v"
+`include "registers_m.v"
 `timescale 1ns / 1ps
 
 module test();
 
 	//Inputs
+	//Decoder
 	reg[31:0] instruction;
 
+	//Registers
+	reg[31:0] writeData;
+
 	//Outputs
+	//Decoder
 	wire Reg2Loc, Uncondbranch, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite; 
 	wire [1:0] ALUOp;
 	wire [4:0] register1, register2, writeRegister;
 	wire signed[31:0] immediate;
 
+	//Registers
+	wire[31:0] data1, data2;
+
 	//Instantiate an object from the decoder class
 	decoder_m decoder(register1, register2, writeRegister, immediate, Reg2Loc,
 			Uncondbranch, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, ALUOp, instruction);
 
-		//Create a waveform file
-		initial begin
-			$dumpfile("waveform.vcd");
-			$dumpvars(0, test);
-		end
+	//Instantiate an object from the register class
+	registers_m register(data1, data2, writeData, register1, register2, writeRegister, immediate, RegWrite, ALUSrc);
 
+	//Create a waveform file
+	initial begin
+		$dumpfile("waveform.vcd");
+		$dumpvars(0, test);
+	end
+
+	//monitors variables
 	initial begin
         $monitor("instruction: %b", instruction,
 			"\tregister1: %b", register1,
@@ -36,7 +49,9 @@ module test();
 			"\tMemToReg: %b", MemtoReg,
 			"\tALUSrc: %b", ALUSrc,
 			"\tRegWrite: %b", RegWrite,
-			"\tALUOp: %b", ALUOp); 
+			"\tALUOp: %b", ALUOp,
+			"\tdata1 :%b", data1,
+			"\tdata2 :%b", data2); 
     end
 
 	initial begin
@@ -59,4 +74,23 @@ module test();
 		//instruction = 111100101_13 bits_00111_00010 
 		#2 instruction = 32'hF28000E2; //MOVE X2, X7
 	end
+
+	/** Test cases from project assignment
+	initial begin
+		//instruction = 11111000010_000000000_00_10100_00001;
+		instruction = 'hF8400281; // LDR X1, [X20, #0]
+		//instruction = 10001011000_00001_000000_00001_00010;
+		#2 instruction = 'h8B010022; ADD X2, X1, X1;
+		//instruction = 1101000100_000000000000_11001_10011;
+		#2 instruction = 'hD1000333; //SUBI X19, X25, #0
+		//instruction = 10110100_0000000000000000111_00011;
+		#2 instruction = 'hB40000E3; //CBZ X3, #7
+		//instruction = 1001000100_000000001000_10100_10100;
+		#2 instruction = 'h91002294; //ADDI X20, X20, #8
+		//instruction = 11111000000_111110100_00_10100_00001;
+		#2 instruction = 'hF81F4281; //X1, [X20, #500]
+		//instruction = 10111111111111111111111111010;
+		#2 instruction = 'h17FFFFFA; // NO OP
+	end
+	**/
 endmodule
