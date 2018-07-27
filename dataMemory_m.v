b854eb8ebfa82e4e2d99268107a99878ea3325a3
Gary Lam <garylam5678@gmail.com>
2018-07-27 11:40:29 -0400

    Updated the data memory to be direct-mapped

--- dataMemory_m.v
+++ dataMemory_m.v
@@ -2,35 +2,58 @@ module dataMemory_m(
 	output reg signed[31:0] readData,
 	input signed[31:0] aluResult, input signed[31:0] writeDataMem, input MemWrite, input MemRead);
 
-	reg[31:0] data_memory [1023:0];//creates an array that holds 1024 32-bit memory blocks and initializes them to 0
-	integer i;
-	initial begin
-		for (i = 0; i < 1024; i = i + 1) begin 
-			data_memory[i] <= 32'b0; 
-		end
-	end
+	reg[30:0] setAddress [0:1023];
+	reg[31:0] setData [0:1023];
 
-	always@* begin
-		if(MemRead == 1) begin//If the instruction is a load
-			if(aluResult % 4 == 0) begin
-				readData <= data_memory[aluResult/4];
-			end	else if(aluResult % 4 == 1) begin
-				readData <= {data_memory[(aluResult-1)/4][31:8], data_memory[(aluResult+3)/4][7:0]};
-			end else if(aluResult % 4 == 2) begin
-				readData <= {data_memory[(aluResult-2)/4][31:16], data_memory[(aluResult+2)/4][15:0]};
-			end else if(aluResult % 4 == 3) begin
-				readData <= {data_memory[(aluResult-3)/4][31:24], data_memory[(aluResult+4)/4][23:0]};
-			end
-		end else if(MemWrite == 1) begin//If the instruction is a write
-			if(aluResult % 4 == 0) begin
-				data_memory[aluResult/4] <= writeDataMem;
-			end else if(aluResult % 4 == 1) begin
-				{data_memory[(aluResult-1)/4][31:8], data_memory[(aluResult+3)/4][7:0]} <= writeDataMem;
-			end else if(aluResult % 4 == 2) begin
-				{data_memory[(aluResult-2)/4][31:16], data_memory[(aluResult+2)/4][15:0]} <= writeDataMem;
-			end else if(aluResult % 4 == 3) begin
-				{data_memory[(aluResult-3)/4][31:24], data_memory[(aluResult+4)/4][23:0]} <= writeDataMem;
-			end
-		end
-	end
+	/**
+	* This cache is a 1024 line direct-map each line with 1 block and 
+	* there are only 32 bits for the data so we have the following:
+	* Byte Offset = 2 bits, Index = 0, Block Offset = 0 bit. Tag = 32 - 2 - 0 = 30.
+	**/
+	wire [30:0] blockAddress = aluResult[31:2];
+	wire [1:0] byteOffset = aluResult[1:0];
+	wire [9:0] blockID  = blockAddress % 1024;
+
+	//Initializes the setData and setAddresses to 0
+	integer i;
+	integer j; // used to loop through the bits in the memory
+	initial begin
+		for(i = 0; i < 1024; i = i + 1) begin
+			setAddress[i] <= 0;
+			setData[i] <= 0;
+		end // for
+	end // initial begin
+
+	always@(MemWrite || MemRead) begin
+		if (setAddress[blockID] == blockAddress) begin // address is in the cache
+			if (MemRead) begin // Read data from the address
+				if (byteOffset != 0 ) begin // if there is a byte offset
+					j = 31;
+					for(i = (31 - 8*byteOffset); i >= 0; i = i - 1) begin
+						readData[j] = setData[blockID][i];
+						j = j - 1;
+					end // for loop
+					for(i = 31; i >= (31 - 8*byteOffset); i = i - 1) begin
+						readData[j] = setData[blockID+1][i];
+						j = j - 1;
+					end // for loop
+				end // if 
+				else begin
+				readData = setData[blockID];
+				end // else
+			end // if
+			else if (MemWrite) begin // Write data to the address 
+				setData[blockID] = writeDataMem;
+			end // else if
+		end //if
+		else begin // address is not in the cache
+			setAddress[blockID] = blockAddress;
+			if (MemWrite) begin
+				setData[blockID] = writeDataMem;
+			end // if
+			else if(MemRead) begin
+				readData = 32'bx;
+			end // else if
+		end // else
+	end // always
 endmodule
